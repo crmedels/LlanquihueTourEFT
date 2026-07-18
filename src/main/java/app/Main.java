e4746a8e5b0a5c7ec8f3b8a3ec7bda54ea3b5067
XCV <cr.medels@duocuc.cl>
2026-07-18 01:00:55 -0400

    Agregar punto de entrada principal de la aplicacion

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+/**
+ * Punto de entrada principal del sistema Llanquihue Tour.
+ *
+ * @author Cristofer Medel
+ */
+public class Main {
+
+    /**
+     * Inicia la ejecucion de la aplicacion.
+     *
+     * @param args argumentos recibidos desde la linea de comandos
+     */
+    public static void main(String[] args) {
+        System.out.println("Llanquihue Tour EFT iniciado correctamente.");
+    }
+}
